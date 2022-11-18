7dc823deade192eef49dbf074489c6c5f266fea3
Kimseonghee <kimsunghee625@gmail.com>
2022-11-18 13:51:04 +0900

    FEAT: 카테고리 페이지 API 파일 생성

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,10 @@ const router = express.Router();
 
 const userRouter = require('./user.router');
 const reviewRouter = require('./review.router');
+const categoryRouter = require('./categorypage.router');
 
 router.use('/user', userRouter);
 router.use('/review', reviewRouter);
+router.use('/category', categoryRouter);
 
 module.exports = router;
