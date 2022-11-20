d19384ae369797d228a0c55bd93a6de840522bfb
c0dject <creseeds@gmail.com>
2022-11-20 11:26:26 +0900

    Refactor: 리뷰 반영 - 컨트롤러단 try-catch문 제거

--- controllers/addList.controller.js
+++ controllers/addList.controller.js
@@ -2,69 +2,49 @@ const addListService = require('../services/addList.service');
 const myUtil = require('../utils/myutil');
 
 const addBookshelf = async (req, res) => {
-  try {
-    const user_id = req.userInfo.id;
-    const { books_id } = req.body;
-    myUtil.checkDataIsNotEmpty({
-      books_id,
-    });
-    const result = await addListService.addBookshelf(user_id, books_id);
-    res.status(200).json({
-      result,
-    });
-    console.log('ADD BOOKSHELF');
-  } catch (err) {
-    console.log(err.message);
-    res.status(err.statusCode).json({ message: err.message });
-  }
+  const user_id = req.userInfo.id;
+  const { books_id } = req.body;
+  myUtil.checkDataIsNotEmpty({
+    books_id,
+  });
+  const result = await addListService.addBookshelf(user_id, books_id);
+  res.status(200).json({
+    result,
+  });
+  console.log('ADD BOOKSHELF');
 };
 
 const removeBookshelf = async (req, res) => {
-  try {
-    const user_id = req.userInfo.id;
-    const { books_id } = req.body;
-    myUtil.checkDataIsNotEmpty({
-      books_id,
-    });
-    const result = await addListService.removeBookshelf(user_id, books_id);
-    res.status(200).json({ message: result });
-  } catch (err) {
-    console.log(err.message);
-    res.status(err.statusCode).json({ message: err.message });
-  }
+  const user_id = req.userInfo.id;
+  const { books_id } = req.body;
+  myUtil.checkDataIsNotEmpty({
+    books_id,
+  });
+  const result = await addListService.removeBookshelf(user_id, books_id);
+  res.status(200).json({ message: result });
 };
 
 const addFavorite = async (req, res) => {
-  try {
-    const user_id = req.userInfo.id;
-    const { books_id } = req.body;
-    myUtil.checkDataIsNotEmpty({
-      books_id,
-    });
-    const result = await addListService.addFavorite(user_id, books_id);
-    res.status(200).json({
-      result,
-    });
-    console.log('ADD FAVORITE');
-  } catch (err) {
-    console.log(err.message);
-    res.status(err.statusCode).json({ message: err.message });
-  }
+  const user_id = req.userInfo.id;
+  const { books_id } = req.body;
+  myUtil.checkDataIsNotEmpty({
+    books_id,
+  });
+  const result = await addListService.addFavorite(user_id, books_id);
+  res.status(200).json({
+    result,
+  });
+  console.log('ADD FAVORITE');
 };
 
 const removeFavorite = async (req, res) => {
-  try {
-    const user_id = req.userInfo.id;
-    const { books_id } = req.body;
-    myUtil.checkDataIsNotEmpty({
-      books_id,
-    });
-    const result = await addListService.removeFavorite(user_id, books_id);
-    res.status(200).json({ message: result });
-  } catch (err) {
-    console.log(err.message);
-    res.status(err.statusCode).json({ message: err.message });
-  }
+  const user_id = req.userInfo.id;
+  const { books_id } = req.body;
+  myUtil.checkDataIsNotEmpty({
+    books_id,
+  });
+  const result = await addListService.removeFavorite(user_id, books_id);
+  res.status(200).json({ message: result });
 };
 
 module.exports = { addBookshelf, removeBookshelf, addFavorite, removeFavorite };
--- controllers/review.controller.js
+++ controllers/review.controller.js
@@ -2,59 +2,40 @@ const reviewService = require('../services/review.service');
 const myUtil = require('../utils/myutil');
 
 async function createReview(req, res) {
-  try {
-    const user_id = req.userInfo.id;
-    console.log('user_id = ', user_id);
-    const { books_id, content } = req.body;
-    // id = 게시물의 id
-    myUtil.checkDataIsNotEmpty({
-      books_id,
-      content,
-    });
-    const result = await reviewService.createReview(user_id, books_id, content);
-    res.status(200).json({
-      result,
-    });
-    console.log('COMMENT POSTED');
-  } catch (err) {
-    console.log(err);
-    res.status(400).json({ message: err.message });
-  }
+  const user_id = req.userInfo.id;
+  console.log('user_id = ', user_id);
+  const { books_id, content } = req.body;
+  // id = 게시물의 id
+  myUtil.checkDataIsNotEmpty({
+    books_id,
+    content,
+  });
+  const result = await reviewService.createReview(user_id, books_id, content);
+  res.status(200).json({
+    result,
+  });
+  console.log('COMMENT POSTED');
 }
 
 const updateReview = async (req, res) => {
-  try {
-    const user_id = req.userInfo.id;
-    const { review_id, content } = req.body;
-    myUtil.checkDataIsNotEmpty({
-      review_id,
-      content,
-    });
-    const result = await reviewService.updateReview(
-      review_id,
-      user_id,
-      content
-    );
-    res.status(200).json({ result });
-  } catch (err) {
-    console.log(err);
-    res.status(400).json({ message: err.message });
-  }
+  const user_id = req.userInfo.id;
+  const { review_id, content } = req.body;
+  myUtil.checkDataIsNotEmpty({
+    review_id,
+    content,
+  });
+  const result = await reviewService.updateReview(review_id, user_id, content);
+  res.status(200).json({ result });
 };
 
 const deleteReview = async (req, res) => {
-  try {
-    const user_id = req.userInfo.id;
-    const { review_id } = req.body;
-    myUtil.checkDataIsNotEmpty({
-      review_id,
-    });
-    const result = await reviewService.deleteReview(review_id, user_id);
-    res.status(200).json({ message: result });
-  } catch (err) {
-    console.log(err.message);
-    res.status(err.statusCode).json({ message: err.message });
-  }
+  const user_id = req.userInfo.id;
+  const { review_id } = req.body;
+  myUtil.checkDataIsNotEmpty({
+    review_id,
+  });
+  const result = await reviewService.deleteReview(review_id, user_id);
+  res.status(200).json({ message: result });
 };
 
 module.exports = { createReview, updateReview, deleteReview };
